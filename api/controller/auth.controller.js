6ff3f1bb3dc6cfba8acbab804a57b66d2a8f7b1f
Furkan Ulutaş <furkanulutas@icloud.com>
2023-10-23 23:52:01 +0300

    feat: error handle created

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,11 +1,12 @@
 ﻿import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
   await newUser.save()
     .then(() => res.json("User created"))
-    .catch((err) => res.status(400).json("Error: " + err));
+    .catch((err) => next(errorHandler(err.status, err.message)));
 };
